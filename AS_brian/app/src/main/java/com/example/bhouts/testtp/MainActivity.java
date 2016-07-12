1f0edbdd8d79e37cf066e3d3632883e56b8717bf
Julia Moffitt <jcmoffit@ucsc.edu>
2016-07-12 08:24:20 -0700

    integrating michaela's menu code and fixed fullscreen activity

---
+++ AS_brian/app/src/main/java/com/example/bhouts/testtp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.bhouts.testtp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newTask(view);
+            }
+        });
+    }
+
+    /** Called when the user clicks the FAB */
+    public void newTask(View view) {
+        Intent intent = new Intent(this, editTask.class);
+        startActivity(intent);
+    }
+
+
+}
