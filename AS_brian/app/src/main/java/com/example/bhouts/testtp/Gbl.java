1b8424a767b246b5e0060092fdeefae649e44fe3
sravnyag <sravnyag@ucsc.edu>
2016-07-11 21:40:18 -0700

    new docs branch

--- AS_brian/app/src/main/java/com/example/bhouts/testtp/Gbl.java
+++
@@ -1,9 +0,0 @@
-package com.example.bhouts.testtp;
-
-/**
- * Created by bhouts on 7/10/2016.
- */
-public class Gbl {
-    public static ProjectList All = new ProjectList();
-    public static Task StartList = new Task("TO DO:", "", ""); //first task will simply show "TO DO:"
-}
