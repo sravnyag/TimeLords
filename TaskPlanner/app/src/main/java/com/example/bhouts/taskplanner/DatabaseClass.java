8c73e4c3f47f473c21f3f4e23476d612b10bb554
swagenseller <jwagense@ucsc.edu>
2016-07-18 16:06:25 -0700

    added DatabaseClass and DatabaseOperations

---
+++ TaskPlanner/app/src/main/java/com/example/bhouts/taskplanner/DatabaseClass.java
@@ -0,0 +1,112 @@
+package com.example.jeffwag.sprint1;
+
+/**
+ * Created by Jeffwag on 7/16/2016.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+// need to import DatabaseOperations
+
+public class DatabaseClass extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "ProjectDB.db";
+
+    private static final String TABLE = "projects";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "projectname";
+    public static final String COLUMN_QUANTITY = "quantity";
+
+
+    public DatabaseClass(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase dB) {
+        String CREATE_TABLE_CLASS = "CREATE TABLE " + TABLE + "(" +
+                COLUMN_ID + " " + "INTEGER PRIMARY KEY," +
+                COLUMN_NAME + " TEXT," +
+                COLUMN_QUANTITY + " INTEGER," + ")";
+
+
+        dB.execSQL(CREATE_TABLE_CLASS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE);
+        onCreate(db);
+    }
+
+    public void addProject(Project mProject) {
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, mProject.getProjectName());
+        values.put(COLUMN_QUANTITY, mProject.getQuantity());
+
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert(TABLE, null, values);
+        db.close();
+    }
+
+
+    public void addQuantity(String name) {
+        String query = "Select * FROM " + TABLE + " WHERE " + COLUMN_NAME + " =  \"" + name + "\"";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.moveToFirst();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_QUANTITY, cursor.getInt(cursor.getColumnIndex(COLUMN_QUANTITY)) + 1);
+
+        db.update(TABLE, values, COLUMN_NAME + " = '" + name + "'", null);
+        cursor.close();
+        db.close();
+    }
+
+    public void changeName(String name, String newname) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, newname);
+
+        db.update(TABLE, values, COLUMN_NAME + " = '" + name + "'", null);
+        db.close();
+    }
+
+
+
+    public void decrementQuantity(String projectname) {
+        String query = "Select * FROM " + TABLE + " WHERE " + COLUMN_NAME + " =  \"" + projectname + "\"";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.moveToFirst();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_QUANTITY, cursor.getInt(cursor.getColumnIndex(COLUMN_QUANTITY)) - 1);
+
+        db.update(TABLE, values, COLUMN_NAME + " = '" + projectname + "'", null);
+        cursor.close();
+        db.close();
+    }
+
+    public Cursor getProjects() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT " + COLUMN_ID + ", " + COLUMN_NAME + ", " + COLUMN_QUANTITY + " FROM " + TABLE;
+        Cursor cursor =  db.rawQuery(selectQuery, null);
+        cursor.moveToFirst();
+        return cursor;
+    }
+
+
+}
---
+++ TaskPlanner/app/src/main/java/com/example/bhouts/taskplanner/DatabaseOperations.java
@@ -0,0 +1,115 @@
+package com.example.jeffwag.sprint1;
+
+/**
+ * Created by Jeffwag on 7/8/2016.
+ *
+ * I am not 100% if this is working properly I made though a few tutorials
+ * and I have a friend that has had experience with databases for android apps
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DatabaseOperations extends SQLiteOpenHelper{
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "taskDB.db";
+    private static final String TABLE = "tasks";
+    public static final String ID = "id";
+    public static final String PROJECT = "project";
+    public static final String TASK_NAME = "taskname";
+    public static final String PROJECT_NAME = "projectname";
+    public static final String NOTES = "notes";
+    public static final String DUE_DATE = "duedate";
+    public static final String COMPLETED = "completed";
+
+    public DatabaseOperations(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE_CLASS = "CREATE TABLE " + TABLE + "(" +
+                ID + " " + "INTEGER PRIMARY KEY," +
+                PROJECT + " " + " TEXT," +
+                TASK_NAME + " TEXT," +
+                PROJECT_NAME + "TEXT," +
+                NOTES + " TEXT," +
+                COMPLETED + "TEXT," +
+                DUE_DATE + " TEXT" + ")";
+
+        db.execSQL(CREATE_TABLE_CLASS);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE);
+        onCreate(db);
+    }
+    public void addTask(Task task) {
+
+        ContentValues values = new ContentValues();
+        values.put(PROJECT, task.getParentClass());
+        values.put(TASK_NAME, task.getName());
+        values.put(PROJECT_NAME, task.getProName());
+        values.put(NOTES, task.getNotes());
+        values.put(DUE_DATE, task.getDueDate());
+        values.put(COMPLETED, task.getComp());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert(TABLE, null, values);
+        db.close();
+    }
+
+    public Cursor getTasks(String Project) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT * FROM " + TABLE + " WHERE " + PROJECT + " = '" + Project + "'";
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        cursor.moveToFirst();
+        return cursor;
+    }
+
+    public void changeTaskName(String name, String newname) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(TASK_NAME, newname);
+        db.update(TABLE, values, TASK_NAME + " = '" + name + "'", null);
+        db.close();
+    }
+
+    public void changeNotes(String notes, String newnotes) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(NOTES, newnotes);
+        db.update(TABLE, values, NOTES + " = '" + notes + "'", null);
+        db.close();
+    }
+
+    public void changeProName(String name, String newname) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(PROJECT_NAME, newname);
+        db.update(TABLE, values, PROJECT_NAME + " = '" + name + "'", null);
+        db.close();
+    }
+
+
+    public void changeProject(String name, String newname) {
+        String query = "Select * FROM " + TABLE + " WHERE " + PROJECT + " =  \"" + name + "\"";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        ContentValues values = new ContentValues();
+        values.put(PROJECT, newname);
+        if(cursor.moveToFirst()) {
+            db.update(TABLE, values, PROJECT + " = '" + name + "'", null);
+        }
+        cursor.close();
+        db.close();
+    }
+
+}
