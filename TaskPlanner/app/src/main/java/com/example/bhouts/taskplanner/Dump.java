137f52decaf25665725b449b912f940a8dd5c0b7
mijmolin <mijmolin@ucsc.edu>
2016-07-24 18:12:49 -0700

    Dump Addition

--- TaskPlanner/app/src/main/java/com/example/bhouts/taskplanner/Dump.java
+++ TaskPlanner/app/src/main/java/com/example/bhouts/taskplanner/Dump.java
@@ -1,13 +1,29 @@
 package com.example.bhouts.taskplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Dump extends AppCompatActivity {
+
+    View.OnClickListener listener2 = null;
+    Button button2;
+
+    /*called when fab button pushed*/
+    public void newTask(View view) {
+        Intent go_ditTask = new Intent(this, editTask.class);
+        startActivity(go_ditTask);
+    }
+
+    //called when fab button pushed/ when project from collapsible list is pushed
+    public void newProject(View view){
+        Intent go_editProject = new Intent(this, editProject.class);
+        startActivity(go_editProject);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +32,32 @@ public class Dump extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+        //declare fab buttons
+        Gbl.fab_task = (FloatingActionButton) findViewById(R.id.fab_task);
+        Gbl.fab_project = (FloatingActionButton) findViewById(R.id.fab_project);
+        Gbl.fab_show = (FloatingActionButton) findViewById(R.id.fab_show);
+        //initially hide these two fabs
+        Gbl.fab_task.hide();
+        Gbl.fab_project.hide();
 
+        listener2 = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Gbl.passProjName = "New Project";
+                if (Gbl.hidden == Boolean.TRUE){
+                    Gbl.fab_task.show();
+                    Gbl.fab_project.show();
+                    Gbl.hidden = Boolean.FALSE;
+                }else {
+                    Gbl.fab_task.hide();
+                    Gbl.fab_project.hide();
+                    Gbl.hidden = Boolean.TRUE;
+                }
+
+            }
+        };
+        Gbl.fab_show.setOnClickListener(listener2);
+
+
+    }
 }
